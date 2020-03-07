cc01564916c002a82c53e8c07b29645ef41459c9
Kerren <kerren@entrostat.com>
2020-03-07 12:25:36 +0200

    added the set results function

--- src/promax.ts
+++ src/promax.ts
@@ -67,6 +67,17 @@ export class Promax<T = any> {
     return Object.assign({}, defaultPromiseLimitOptions, options);
   }
 
+  /**
+   * Allows you to set the results array by reference (if you want to
+   * chain all of your commands together)
+   * @param results
+   */
+  public setResults(results: any) {
+    results.valid = [];
+    results.invalid = [];
+    this.results = results;
+  }
+
   public add(func: PromiseFunction<T>): Promax;
   public add(func: PromiseFunctionWithArgs<T>, ...args: any[]): Promax;
   // tslint:disable-next-line:unified-signatures
